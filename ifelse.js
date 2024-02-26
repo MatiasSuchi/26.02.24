6095bc962714143e286262e1f7a9169f156b2f37
Giovani <142435797+MatiasSuchi@users.noreply.github.com>
2024-02-26 12:23:17 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,20 @@
+// console.log("Hello World!");
+
+// a linha a seguir e uma declaracao da variavel "x" que receber valor 2 e termina com ";"
+var x = 2;
+
+// a linha a seguir e uma declaracao da variavel "y" que receber valor 2 e termina com ";"
+var y = 2;
+
+// a linha abaixo e uma declaracao do algoritmo if/else, cuja condiçao e: se a variavel "x" for igual a variavel "y", sera exibido no log do console a mensagem "Numero conferem", com contrario (else) sera exibido no console a mensagem "Numeros divergem"
+if (x == y) {
+
+//  eu sei que esta mandando o "numeros conferem" pro console
+    console.log("Números conferem!");
+
+// nao sei
+} else {
+
+// eu sei que esta mandando o "numeros divergem" para o console
+    console.log("Números divergem!");
+}
